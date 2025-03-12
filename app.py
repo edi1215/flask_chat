88bdd18ff1dc1a93ad8339ffa2bd5debf9ce34cb
Tal Koosh <talkoosh@Tals-MacBook-Air.local>
2025-03-12 12:10:11 +0200

    add mock dir and send_from_directory

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, current_app
+from flask import Flask, request, current_app, send_from_directory
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def get_room(room):
 
 @app.route('/api/chat/<room>')
 def get_room_chat(room): 
-        return 
+        return send_from_directory("mock/rooms", f"{room}")
 
 
 if __name__ == '__main__':
