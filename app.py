ef3f7e0d8526071e5ec8ff2cd09adce0c7b2fe87
Eddy Sraiber <edi1215@gmail.com>
2025-03-12 11:03:22 +0200

    render for get room(do the same as get without room provided in the url

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return current_app.send_static_file('index.html')
 
+
+@app.route('/<room>', methods=['GET'])
+def getroom(room):
+    return render_template("index.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
